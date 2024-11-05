03a56fab91d1115962fb28db8d3ddf13ce27cf65
Zetia <101085187+Zx5xd@users.noreply.github.com>
2024-11-06 03:12:18 +0900

    tailwind,postcss config 추가

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 프로젝트 내 모든 파일에서 Tailwind CSS를 사용 가능하게 설정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
